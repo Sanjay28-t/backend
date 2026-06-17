9a867ea65a9b8203e5244d728b2aecc4bb4ad8e9
Sanjay28-t <sanjaykumarnonia567@gmail.com>
2026-06-17 19:55:16 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ app.get('/twitter', (req, res) =>{
 app.get('/login', (req, res) =>{
     res.send('<h1>please login my page</h1>')
 })
-
+app.get('/git',(req,res) =>{
+  res.send('cse student')
+})
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
 })
